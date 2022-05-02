e89c3a3bfcbc997a7ff6360da094ff838bb9db25
JumpyLionnn <fwipsity@gmail.com>
2022-05-02 15:40:03 +0300

    added a premake file

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+	staticruntime "On"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h"
+    }
+
+    includedirs 
+    {
+        "include"
+    }
+
+    filter "system:windows"
+		systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+        runtime "Release"
+		optimize "On"
